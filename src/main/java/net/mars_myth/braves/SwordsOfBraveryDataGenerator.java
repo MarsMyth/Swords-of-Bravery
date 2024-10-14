538e65c98ab3a76ec8e903cd7f25aad1ba7c3fb8
MarsMyth <callumshelton2503@gmail.com>
2024-10-14 19:47:35 +1000

    Initial commit

---
+++ src/main/java/net/mars_myth/braves/SwordsOfBraveryDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mars_myth.braves;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SwordsOfBraveryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
